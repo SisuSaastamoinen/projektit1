53f0a21296e0a3b2ddf2e5a62dcefa91d2efbc4b
niko.muukkonen <niko.muukkonen@edu.keuda.fi>
2025-10-29 19:35:27 +0200

    fix bugs

--- arvauspeli/src/arvauspeli.js
+++ arvauspeli/src/arvauspeli.js
@@ -1,40 +1,46 @@
 let correct = Math.floor(Math.random() * 10) + 1;
 const maxGuesses = 3;
+const GAME_END = 999;
 let timesGuessed = 0;
 let wins = 0;
 let losses = 0;
+let gameInfo = document.getElementById("gameInfo");
 
 function guess(event) {
     const userGuess = parseInt(event.target.value);
-    if (timesGuessed >= maxGuesses) {
-        alert("Peli on jo päättynyt! Aloita uusi peli.");
+    if (timesGuessed === GAME_END) {
+        return;
+    }
+    if (timesGuessed === maxGuesses) {
+        gameInfo.innerHTML = `Peli ohi! Oikea numero oli ${correct}. Aloita uusi peli.`;
         return;
     }
     timesGuessed++;
-    document.getElementById("arvaukset").innerHTML = "Arvausten lukumäärä: " + timesGuessed;
+    document.getElementById("arvaukset").innerHTML = `Arvausten lukumäärä: ${timesGuessed}`;
     let guessedNumbers = document.getElementById("arvatutNumerot").innerHTML;
     guessedNumbers += userGuess + " ";
     document.getElementById("arvatutNumerot").innerHTML = guessedNumbers;
     if (userGuess === correct) {
         wins++;
-        document.getElementById("voitot").innerHTML = "Voitot: " + wins;
-        alert("Arvasit oikein! Oikea numero oli " + correct + ".");
-        resetGame();
+        document.getElementById("voitot").innerHTML = `Voitot: ${wins}`;
+        gameInfo.innerHTML = `Arvasit oikein! Oikea numero oli ${correct}.`;
+        timesGuessed = GAME_END; 
+        return;
     } else {
         if (timesGuessed === maxGuesses) {
             losses++;
-            document.getElementById("tappiot").innerHTML = "Tappiot: " + losses;
-            alert("Peli päättyi! Oikea numero oli " + correct + ".");
-            resetGame();
+            document.getElementById("tappiot").innerHTML = `Tappiot: ${losses}`;
+            gameInfo.innerHTML = `Peli ohi! Oikea numero oli ${correct}.`;
         } else {
-            alert("Väärin! Yritä uudelleen. Sinulla on " + (maxGuesses - timesGuessed) + " arvausta jäljellä.");
+            gameInfo.innerHTML = `Väärin! Yritä uudelleen. Sinulla on ${maxGuesses - timesGuessed} arvausta jäljellä.`;
         }
     }
 }
 
 function resetGame() {
+    gameInfo.innerHTML = "Uusi peli aloitettu! Arvaa numero väliltä 1-10.";
     correct = Math.floor(Math.random() * 10) + 1;
     timesGuessed = 0;
-    document.getElementById("arvaukset").innerHTML = "Arvausten lukumäärä: " + timesGuessed;
+    document.getElementById("arvaukset").innerHTML = `Arvausten lukumäärä: ${timesGuessed}`;
     document.getElementById("arvatutNumerot").innerHTML = "Arvatut numerot: ";
 }
