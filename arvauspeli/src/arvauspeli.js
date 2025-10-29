603c4bcfd7da7ee20b73a6d93c66787533417b50
niko.muukkonen <niko.muukkonne@edu.keuda.fi>
2025-10-29 13:17:31 +0200

    make skeleton

---
+++ arvauspeli/src/arvauspeli.js
@@ -0,0 +1,7 @@
+function guess() {
+    const userInput = document.getElementsByName("userGuess"); 
+    const correct = Math.floor(Math.random() * 10) + 1;
+    let guesses = [];
+    let wins = 0;
+    let losses = 0;
+}
