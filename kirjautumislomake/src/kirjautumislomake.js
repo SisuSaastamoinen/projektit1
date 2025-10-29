c0a6e8c2bf971f016ca0798ab0b92905a3491733
SisuSaastamoinen <sisu.saastamoinen@edu.keuda.fi>
2025-10-29 11:19:52 +0200

    vaihdoin getelementbyid muotoon# Please enter the commit message for your changes. Lines starting

--- kirjautumislomake/src/kirjautumislomake.js
+++ kirjautumislomake/src/kirjautumislomake.js
@@ -1,34 +1,34 @@
 function validateForm() {
-    let x = document.forms["username"].value;
+    let x = document.getElementById["username"].value;
     if (x == "") {
         alert("Nimi ei voi olla tyhjä.")
         return false;
     }
-    let y = document.forms["password"].value;
+    let y = document.getElementById["password"].value;
     if (y == "") {
         alert("Salasana ei voi olla tyhjä.")
         return false;
     }
-    let a1 = document.forms["userOptionRadio1a"].value;
-    let a2 = document.forms["userOptionRadio2a"].value;
-    let a3 = document.forms["userOptionRadio3a"].value;
+    let a1 = document.getElementById["userOptionRadio1a"].value;
+    let a2 = document.getElementById["userOptionRadio2a"].value;
+    let a3 = document.getElementById["userOptionRadio3a"].value;
     if (a1 == unchecked && a2 == unchecked && a3 == unchecked) {
         alert("Valitse käyttäjä tyyppi.")
         return false;
     }
-    let b1 = document.forms["userOptionCheckbox1b"].value;
-    let b2 = document.forms["userOptionCheckbox2b"].value;
-    let b3 = document.forms["userOptionCheckbox3b"].value;
+    let b1 = document.getElementById["userOptionCheckbox1b"].value;
+    let b2 = document.getElementById["userOptionCheckbox2b"].value;
+    let b3 = document.getElementById["userOptionCheckbox3b"].value;
     if (b1 == unchecked && b2 == unchecked && b3 == unchecked) {
         alert("Täytä mielipide.")
         return false;
     }
-    let q = document.forms["parannusehdotukset"].value;
+    let q = document.getElementById["parannusehdotukset"].value;
     if (q == "") {
         alert("Kirjoitsa parannusehdotus.")
         return false;
     }
-    let w = document.forms["lempiaine"].value;
+    let w = document.getElementById["lempiaine"].value;
     if (w == "eiValintaa") {
         alert("Valitse lempi aine.")
         return false;
