6a7e867edda94dbb5f7ae7b6788558b933b43074
SisuSaastamoinen <sisu.saastamoinen@edu.keuda.fi>
2025-10-29 11:45:55 +0200

    laitoin ne loput ;

--- kirjautumislomake/src/kirjautumislomake.js
+++ kirjautumislomake/src/kirjautumislomake.js
@@ -2,35 +2,35 @@ function validateForm() {
     let x = document.getElementById("username").value;
     if (x == "") {
         alert("Nimi ei voi olla tyhjä.")
-        return 
+        return; 
     }
     let y = document.getElementById("password").value;
     if (y == "") {
         alert("Salasana ei voi olla tyhjä.")
-        return 
+        return; 
     }
     let a1 = document.getElementById("userOptionRadio1a").checked;
     let a2 = document.getElementById("userOptionRadio2a").checked;
     let a3 = document.getElementById("userOptionRadio3a").checked;
     if (!a1 && !a2 && !a3) {
         alert("Valitse käyttäjä tyyppi.")
-        return
+        return;
     }
     let b1 = document.getElementById("userOptionCheckbox1b").checked;
     let b2 = document.getElementById("userOptionCheckbox2b").checked;
     let b3 = document.getElementById("userOptionCheckbox3b").checked;
     if (!b1 && !b2 && !b3) {
         alert("Täytä mielipide.")
-        return
+        return;
     }
     let q = document.getElementById("parannusehdotukset").value;
     if (q == "") {
         alert("Kirjoita parannusehdotus.")
-        return
+        return;
     }
     let w = document.getElementById("lempiaine").value;
     if (w == "eiValintaa") {
         alert("Valitse lempi aine.")
-        return
+        return;
     }
 }
