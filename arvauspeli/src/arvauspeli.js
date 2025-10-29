5163eb98d7f825cb88a849e446a13d953c80cbd2
niko.muukkonen <niko.muukkonne@edu.keuda.fi>
2025-10-29 13:34:00 +0200

    use events

--- arvauspeli/src/arvauspeli.js
+++ arvauspeli/src/arvauspeli.js
@@ -1,7 +1,8 @@
-function guess() {
-    const userInput = document.getElementsByName("button");
-    console.log(userInput);
+function guess(event) {
+    const userGuess = event.target.value;
+    console.log(userGuess);
     const correct = Math.floor(Math.random() * 10) + 1;
+    console.log(correct);
     let guesses = [];
     let wins = 0;
     let losses = 0;
