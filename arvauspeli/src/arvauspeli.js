19281c45ae112b9eb25ed8d3bbdd9279f9c076ae
niko.muukkonen <niko.muukkonne@edu.keuda.fi>
2025-10-29 13:26:22 +0200

    add stylesheet

--- arvauspeli/src/arvauspeli.js
+++ arvauspeli/src/arvauspeli.js
@@ -1,5 +1,6 @@
 function guess() {
-    const userInput = document.getElementsByName("userGuess"); 
+    const userInput = document.getElementsByName("button");
+    console.log(userInput);
     const correct = Math.floor(Math.random() * 10) + 1;
     let guesses = [];
     let wins = 0;
