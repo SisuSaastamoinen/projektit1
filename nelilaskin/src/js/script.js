f33627c5795ae0b1610d25106884994088849968
niko.muukkonen <niko.muukkonne@edu.keuda.fi>
2025-10-30 12:40:07 +0200

    createButtons works

--- nelilaskin/src/js/script.js
+++ nelilaskin/src/js/script.js
@@ -1,9 +1,11 @@
+createButtons();
 function createButtons() {
     const buttonLabels = ['AC','C','%','/','7','8','9','*','4','5','6','-','1','2','3','+','0',',','=']
     let buttonContainer = document.getElementById("buttonContainer");
     for (let i=0;i<buttonLabels.length; i++) {
-        if (i % 3 == 0) {
-            buttonContainer.appendChild("<br>");
+        if (i % 4 == 0) {
+            const br = document.createElement("br")
+            buttonContainer.appendChild(br);
         }
         let btn = document.createElement('button');
         btn.value = buttonLabels[i];
@@ -15,5 +17,6 @@ function createButtons() {
 }
 
 function parseExpression(event) {
-
+    const numberDisplay = document.getElementById("numberDisplay");
+    const input = event.target.value;
 }
