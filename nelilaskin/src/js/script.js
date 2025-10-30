731c91d7fe63bd8f3d6720dcc0380273feb07883
niko.muukkonen <niko.muukkonne@edu.keuda.fi>
2025-10-30 13:27:32 +0200

    add styles and some classes to js

--- nelilaskin/src/js/script.js
+++ nelilaskin/src/js/script.js
@@ -1,3 +1,4 @@
+let expression = "";
 createButtons();
 function createButtons() {
     const buttonLabels = ['AC','C','%','/','7','8','9','*','4','5','6','-','1','2','3','+','0',',','=']
@@ -10,13 +11,52 @@ function createButtons() {
         let btn = document.createElement('button');
         btn.value = buttonLabels[i];
         btn.innerHTML = buttonLabels[i];
-        btn.onclick = parseExpression;
-        btn.className = "calcButton";
+        btn.onclick = (event) => parseExpression(event);
+        if (btn.value === '0') {
+            btn.className = " zeroButton";
+        } else {
+            btn.className = "calcButton";
+        }
         buttonContainer.appendChild(btn);
     }
 }
 
 function parseExpression(event) {
-    const numberDisplay = document.getElementById("numberDisplay");
-    const input = event.target.value;
+    console.log(event.target.value);
+    let calculatorContainer = document.getElementById("calculatorContainer");
+    const input = classifyInput(event.target.value);
+    if (input.value === '=') {
+        
+        return;
+    }
+    calculatorContainer.innerHTML += input.value;
+}
+
+function classifyInput(input) {
+    const operators = ['+','-','*','/','%','='];
+    if (!isNaN(input)) {
+        return new numberInput(input);
+    } else if (operators.includes(input)) {
+        return new operatorInput(input);
+    } else {
+        return new editingInput(input)  ;
+    }
+}
+
+class numberInput {
+    constructor(value) {
+        this.value = value;
+    }   
+}
+
+class operatorInput {
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+class editingInput {
+    constructor(value) {
+        this.value = value;
+    }   
 }
