46d984f648fd7d754e3af152fa1e1c454f08bde9
SisuSaastamoinen <sisu.saastamoinen@edu.keuda.fi>
2025-11-03 12:52:55 +0200

    tein alkua

--- tehtavalista/src/js/script.js
+++ tehtavalista/src/js/script.js
@@ -1,5 +1,7 @@
 let todoList = [];
 let input = document.getElementById("todoInput").value;
+var enterButton = document.getElementById("submitButton");
+enterButton.addEventListener("click",addToList);
 
 function addToListAfterEnterPressed() {
   if (checkInputLength() > 0 && event.which == 13) {
@@ -20,3 +22,17 @@ function checkInputLength() {
 function checkTodoListLength() {
   return todoList.length();
 }
+
+function createNewTodo() {
+  var li = document.createElement("li");
+  {
+    li.appendChild(document.createTextNode(input.value));
+  	ul.appendChild(li);
+  	input.value = "";
+}
+}
+
+var del = document.createElement("button"); 
+del.appendChild(document.createTextNode("X"));
+li.appendChild(del);
+del.addEventListener("click", delTask);
