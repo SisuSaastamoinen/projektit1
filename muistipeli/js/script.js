06b3f568039019f03884c8c81afdcfa0d0eecf6e
Niukkis2 <basanaama@gmail.com>
2025-11-05 14:03:48 +0200

    create picture elements and assign onclick function to them

--- muistipeli/js/script.js
+++ muistipeli/js/script.js
@@ -1,11 +1,27 @@
-suuruus() {
+let board = [];
+createBoard();
 
+function onClickPicture() {
+  console.log("onclick toimii!")
 }
 
-resetoi(){
-
-}
-
-aloita(){
-    
+function createBoard() {
+  /*TODO: 
+  * lataa kuvat, lyö ne listaan ja sekoita,
+  * jotta ne saadaan elementteihin
+  * */
+  let pictureIdAppendix = 0;
+  let pictures = [];
+  for (let i = 1; i <= 6; i++) {
+    let newTr = document.createElement("tr");
+    newTr.setAttribute("id", "row" + i);
+    for (let j = 1; j <= 6; j++) {
+      let newTd = document.createElement("td");
+      newTd.setAttribute("id", "pic" + pictureIdAppendix);
+      newTd.onclick = onClickPicture;
+      newTr.appendChild(newTd);
+      pictureIdAppendix++;
+    }
+    document.getElementById("boardContainer").appendChild(newTr);
+  }
 }
