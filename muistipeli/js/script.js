7905acc6c1fc7b18b695c89e7c842bf3fdb66f06
Niukkis2 <basanaama@gmail.com>
2025-11-06 14:38:43 +0200

    use programming related images

--- muistipeli/js/script.js
+++ muistipeli/js/script.js
@@ -32,47 +32,28 @@ function startGame() {
 function assignImagesToBoard() {
   for (let i = 1, j = 19; i <= 18, j <= 36; i++, j++) {
     const img1 = document.createElement("img");
-    img1.src = "../resources/img/monke" + i + ".jpg";
+    img1.src = "./resources/img/dev_icons/dev" + i + ".jpg";
     const img2 = document.createElement("img");
-    img2.src = "../resources/img/monke" + i + ".jpg";
+    img2.src = "./resources/img/dev_icons/dev" + i + ".jpg";
     document.getElementById("pic" + i).appendChild(img1);
     document.getElementById("pic" + j).appendChild(img2);
   }
 }
 
 function createBoard() {
-  //function assignImagesToBoard() {
-  //  for (let i = 1, j = 18; i <= 18, j <= 36; i++) {
-  //    const img1 = document.createElement("img");
-  //    img1.src = "../resources/img/monke" + i + ".jpg";
-  //    const img2 = document.createElement("img");
-  //    img2.src = "../resources/img/monke" + i + ".jpg";
-  //    document.getElementById("pic" + i).appendChild(img1);
-  //    document.getElementById("pic" + i).appendChild(img2);
-  //  }
-  //}
-
-  function createBoard() {
-    /*TODO: 
-    * lataa kuvat netistä, lyö ne listaan ja sekoita,
-    * jotta ne saadaan elementteihin,
-    * joudutaan tod.näk laittamaan ne oikean kokoisiksi ensin,
-    * tiedostopolku on "../resources/img/monkeXX.jpg"
-    * */
-    let pictureIdAppendix = 1;
-    for (let i = 1; i <= 6; i++) {
-      let newTr = document.createElement("tr");
-      newTr.setAttribute("id", "row" + i);
-      for (let j = 1; j <= 6; j++) {
-        let newTd = document.createElement("td");
-        newTd.setAttribute("id", "pic" + pictureIdAppendix);
-        newTd.onclick = onClickPicture;
-        newTr.appendChild(newTd);
-        pictureIdAppendix++;
-      }
-      let boardContainer = document.getElementById("boardContainer");
-      boardContainer.appendChild(newTr);
-      board = boardContainer;
+  let pictureIdAppendix = 1;
+  for (let i = 1; i <= 6; i++) {
+    let newTr = document.createElement("tr");
+    newTr.setAttribute("id", "row" + i);
+    for (let j = 1; j <= 6; j++) {
+      let newTd = document.createElement("td");
+      newTd.setAttribute("id", "pic" + pictureIdAppendix);
+      newTd.onclick = onClickPicture;
+      newTr.appendChild(newTd);
+      pictureIdAppendix++;
     }
+    let boardContainer = document.getElementById("boardContainer");
+    boardContainer.appendChild(newTr);
+    board = boardContainer;
   }
 }
