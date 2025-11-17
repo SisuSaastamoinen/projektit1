0b10ae1033b0972f111c47215da69aa34e654157
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-17 12:28:25 +0200

    unnessecary code

--- elainaanet/js/animals.js
+++ elainaanet/js/animals.js
@@ -1,15 +1,6 @@
-async function playSound(animal) {
-  console.log(animal.id);
-  const url = `https://keudaope.github.io/HTML-CSS-JS/JavaScript-main/projektit/p7/sounds/${animal.id}.mp3`;
-  try {
-    await fetch(url)
-      .then((res) => res.blob())
-      .then((blob) => {
-        const audioUrl = URL.createObjectURL(blob);
-        const audio = new Audio(audioUrl);
-        audio.play();
-      });
-  } catch (error) {
-    console.log(error);
-  }
+function playSound(animal) {
+  var audio = new Audio(
+    `https://keudaope.github.io/HTML-CSS-JS/JavaScript-main/projektit/p7/sounds/${animal.id}.mp3`,
+  );
+  audio.play();
 }
