f0dbbd44f355733bc4eb49191cf16e238ae66a9f
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-24 09:58:42 +0200

    logic works

--- ksp/js/ksp.js
+++ ksp/js/ksp.js
@@ -4,18 +4,18 @@ let player_choice = null;
 let computer_choice = null;
 let player_score = 0;
 let computer_score = 0;
-const elements = ["rock", "paper", "scissors"];
 
-const win_msg = win_msg;
-const lose_msg = lose_msg;
-const tie_msg = "It's a tie!";
-/* Message format:
- * key: playerChoice_computerChoice
- * value: message to display
- */
+function onClick(event) {
+  const win_msg = "You win!";
+  const lose_msg = "You lose!";
+  const tie_msg = "It's a tie!";
+  /* Message format:
+   * key: playerChoice_computerChoice
+   * value: message to display
+   */
 
-//prettier-ignore
-const messages = {
+  //prettier-ignore
+  const messages = {
 	rock_paper:				 lose_msg,
 	rock_scissors:     win_msg,
 	rock_rock:				 tie_msg,
@@ -26,11 +26,12 @@ const messages = {
 	scissors_paper:		 win_msg,
 	scissors_scissors: tie_msg,
 };
-
-function onClick(event) {
   let element = event.target.id;
-  player_choice = element.id;
+  player_choice = element;
   computer_choice = getComputerChoice();
+  // console.log(
+  //   `Player choice: ${player_choice}, Computer choice: ${computer_choice}`,
+  // );
   const messageKey = `${player_choice}_${computer_choice}`;
   const message = messages[messageKey];
   document.querySelector(".game-result").innerText = message;
@@ -43,7 +44,9 @@ function addListeners() {
 }
 
 function getComputerChoice() {
+  const elements = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * 3);
   const chosen = elements[randomIndex];
-  return document.querySelector("." + chosen).innerText;
+  console.log(document.querySelector("#" + chosen));
+  return document.querySelector("#" + chosen).id;
 }
