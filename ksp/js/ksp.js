b67f44d2ef944132ae535b5fac8445806f45a411
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-24 11:49:47 +0200

    change result message to fetch translation

--- ksp/js/ksp.js
+++ ksp/js/ksp.js
@@ -27,20 +27,31 @@ function onClick(event) {
 	scissors_paper:		 win_msg,
 	scissors_scissors: tie_msg,
 };
+
+  const translations = {
+    rock: "Kivi",
+    paper: "Paperi",
+    scissors: "Sakset",
+  };
   const element = event.target.id;
   player_choice = element;
   computer_choice = getComputerChoice();
   const messageKey = `${player_choice}_${computer_choice}`;
   const message = messages[messageKey];
+  let message_to_display = "";
   if (message === win_msg) {
+    message_to_display = `${translations[player_choice]} on voimakkaampi kuin ${translations[computer_choice]}. Sinä voitit!`;
     player_score++;
   } else if (message === lose_msg) {
+    message_to_display = `${translations[computer_choice]} on voimakkaampi kuin ${translations[player_choice]}. Sinä hävisit!`;
     computer_score++;
+  } else {
+    message_to_display = "Tasapeli";
   }
   document.querySelector(".player-score").innerText = player_score;
   document.querySelector(".computer-score").innerText = computer_score;
   console.log(document.querySelector(".game-result"));
-  document.querySelector("#game-result").innerText = message;
+  document.querySelector("#game-result").innerText = message_to_display;
 }
 
 function addListeners() {
