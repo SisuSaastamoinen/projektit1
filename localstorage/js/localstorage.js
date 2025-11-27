0c00267b467e69ac15c76597b7b1433edf0ad738
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-27 13:07:30 +0200

    add onclicks and listeners
    TODO: make save functionality

--- localstorage/js/localstorage.js
+++ localstorage/js/localstorage.js
@@ -3,19 +3,39 @@ function clearUserData() {
 }
 
 function saveUserData() {
-    const userData = {
-        Etunimi: document.getElementById("first-name").value,
-        Sukunimi: document.getElementById("last-name").value,
-        Osoite: document.getElementById("address").value,
-        Postinumero: document.getElementById("postcode").value,
-        Toimipaikka: document.getElementById("post-area").value,
-        Puhelin: document.getElementById("phone").value,
-        Email: document.getElementById("email").value
-    };
-    localStorage.setItem(document.getElementById("last-name"), JSON.stringify(userData));
-};
+  const userData = {
+    Etunimi: document.getElementById("first-name").value,
+    Sukunimi: document.getElementById("last-name").value,
+    Osoite: document.getElementById("address").value,
+    Postinumero: document.getElementById("postcode").value,
+    Toimipaikka: document.getElementById("post-area").value,
+    Puhelin: document.getElementById("phone").value,
+    Email: document.getElementById("email").value,
+  };
+  localStorage.setItem(
+    document.getElementById("last-name"),
+    JSON.stringify(userData),
+  );
+}
+
+function onClick(event) {
+  saveUserData();
+}
+
+function saveUserData() {
+  //TODO
+}
+
+function addListeners() {
+  document.querySelectorAll(".storage-button").forEach((button) => {
+    button.addEventListener("click", onClick);
+  });
+}
 
 function loadUserData() {
-   const lUser = JSON.parse(localStorage.getItem(document.getElementById("input-search-query")));
-    console.log(lUser);
-};
+  const lUser = JSON.parse(
+    localStorage.getItem(document.getElementById("input-search-query")),
+  );
+  console.log(lUser);
+}
+
