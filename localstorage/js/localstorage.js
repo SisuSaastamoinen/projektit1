a556806c86fa55626b987358b5b79f78339db6ac
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-27 12:47:23 +0200

    fix centering elements

--- localstorage/js/localstorage.js
+++ localstorage/js/localstorage.js
@@ -1,14 +1,19 @@
-
 function clearUserData() {
-    localStorage.clear()
-};
+  localStorage.clear();
+}
 
 function saveUserData() {
-    const userData = {}
-    localStorage.setItem(document.getElementById("last-name"), JSON.stringify(userData));
+  const userData = {};
+  localStorage.setItem(
+    document.getElementById("last-name"),
+    JSON.stringify(userData),
+  );
 }
 
 function loadUserData() {
-   const lUser = JSON.parse(localStorage.getItem(document.getElementById("input-search-query")));
-    console.log(lUser);
+  const lUser = JSON.parse(
+    localStorage.getItem(document.getElementById("input-search-query")),
+  );
+  console.log(lUser);
 }
+
