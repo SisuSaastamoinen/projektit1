bef22d91b7a447cc831de20f4d0a8194c14d9aa7
SisuSaastamoinen <sisu.saastamoinen@edu.keuda.fi>
2025-11-27 12:51:29 +0200

    did stuff

--- localstorage/js/localstorage.js
+++ localstorage/js/localstorage.js
@@ -4,11 +4,19 @@ function clearUserData() {
 };
 
 function saveUserData() {
-    const userData = {}
+    const userData = {
+        Etunimi: document.getElementById("first-name").value,
+        Sukunimi: document.getElementById("last-name").value,
+        Osoite: document.getElementById("address").value,
+        Postinumero: document.getElementById("postcode").value,
+        Toimipaikka: document.getElementById("post-area").value,
+        Puhelin: 
+
+    }
     localStorage.setItem(document.getElementById("last-name"), JSON.stringify(userData));
-}
+};
 
 function loadUserData() {
    const lUser = JSON.parse(localStorage.getItem(document.getElementById("input-search-query")));
     console.log(lUser);
-}
+};
