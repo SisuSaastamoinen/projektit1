48f46cabe409bb27df24c7b299c4c87373c1c759
Niko Muukkonen <niko.muukkonen@edu.keuda.fi>
2025-11-27 11:54:05 +0200

    add search by key and delete by key html

---
+++ localstorage/js/localstorage.js
@@ -0,0 +1,11 @@
+function addItem() {
+  const userData = {
+    first_name: "",
+    last_name: "",
+  };
+  const firstNameValue = document.querySelector("#first-name").value;
+  const firstNameKey = "first_name";
+  userData[firstNameKey] = firstNameValue;
+  const userStringified = JSON.stringify(userData);
+  JSON.parse(userStringified);
+}
